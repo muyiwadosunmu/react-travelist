ce0a10a283bcdccb31b8295591071223cd108ef8
Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-07-26 12:19:58 +0100

    Trave list project

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    //A function that adds an items to an existing array
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              packed: !item.packed,
+            }
+          : item
+      )
+    );
+  }
+
+  function handleClearlIist() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete allitems?"
+    );
+    confirmed ? setItems([]) : null;
+  }
+
+  return (
+    <>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onClearList={handleClearlIist}
+      />
+      <Stats items={items} />
+    </>
+  );
+}
+
+export default App;
